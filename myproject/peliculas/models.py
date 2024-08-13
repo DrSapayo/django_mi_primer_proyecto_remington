d7cde3bda6b1fa8239adbe54ad51324700973c3b
Juan David Triana Turmeque <88797895+JuanDavidTriana@users.noreply.github.com>
2024-08-12 19:08:30 -0500

    Model Peliculas

--- myproject/peliculas/models.py
+++ myproject/peliculas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Peliculas(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=150, verbose_name='Titulo')
+    imagen = models.ImageField(upload_to='imagenes/', verbose_name='Imagen', null= True, blank=True)
+    descripcion = models.TextField(null=True, verbose_name='Descripcion')
