702a95aa6525de1180dadf10f85e30bda66ecbdd
Juan David Triana Turmeque <88797895+JuanDavidTriana@users.noreply.github.com>
2024-08-12 20:42:52 -0500

    rest_framework

--- myproject/peliculas/views.py
+++ myproject/peliculas/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Peliculas
+from .serializers import PeliculasSerializer
 
 # Create your views here.
+class PeliculasViewSet(viewsets.ModelViewSet):
+    queryset = Peliculas.objects.all()
+    serializer_class = PeliculasSerializer
